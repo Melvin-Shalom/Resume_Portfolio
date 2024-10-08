d8154ce98ea902fea65418f3127b813887197045
Melvin-Shalom <okshalommelvin@gmail.com>
2024-10-08 11:24:12 +0530

    Issues Fixed

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
 (function($) {
-  "use strict"; // Start of use strict
-  //The purpose of "use strict" is to indicate that the code should be executed in "strict mode".
-  //With strict mode, you can not, for example, use undeclared variables (lessprone to errors)
-  // Smooth scrolling using jQuery easing
+  "use strict";
   $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
     if (
       location.pathname.replace(/^\//, "") ==
@@ -24,15 +21,17 @@
       }
     }
   });
-  // Closes responsive menu when a scroll trigger link is clicked
+
   $(".js-scroll-trigger").click(function() {
     $(".navbar-collapse").collapse("hide");
   });
-  // Activate scrollspy to add active class to navbar items on scroll
+
   $("body").scrollspy({
     target: "#sideNav",
   });
-})(jQuery); // End of use strict
+})(jQuery);
+
+
 //dark mode
 function make_me_dark() {
   var element = document.body;
